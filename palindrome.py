7beb16a9e8b0f322a35a544195a6b665bb97d142
PJ Passalacqua <pjpassalacqua@Daniels-MacBook-Pro.local>
2015-05-27 15:21:16 -0400

    implemented palindrom_check and reformat into full program

--- palindrome.py
+++ palindrome.py
@@ -14,22 +14,36 @@ def palindrome_check_recursive(string_):
     if string_.endswith(string_[0]):
         print("First and last characters are the same.")
         print("Stripping first and last characters from the string.")
-        return palindrome_check(string_[1:-1])
+        return palindrome_check_recursive(string_[1:-1])
     print("First and last characters are different.")
     return False
 
 # Takes a string and returns all of the letters in lower case.
 def reformat(string_):
+    print("Removing all non-letters.")
     string_ = re.sub(r'[^A-Za-z]','',string_)
+    print("Making string lower case.")
     string_ = string_.lower()
     return string_
 
 
 # Get user input.
+potential_palindrome_input = input("Enter a string > ")
+
+if not potential_palindrome_input:
+    print ("You didn't enter anything.")
+    exit()
 
 # Remove spaces, punctuation, and capitalization from the input
+potential_palindrome = reformat(potential_palindrome_input)
 
 # Algorithm
-    # Check to see if first and last characters are the same
-        # If they are, continue
-        # If they aren't, break
+    # Compare first and last letter.
+    # If they are the same, then compare next letters
+print("Checking to see if you entered a palindrome.")
+is_a_palindrome = palindrome_check_recursive(potential_palindrome)
+
+if is_a_palindrome:
+    print("{} is a palindrome.".format(potential_palindrome_input))
+else:
+    print("{} is not a palindrome.".format(potential_palindrome))
