6d88b6a5d1c82c521dad35f12cf219f4cb5d7840
PJ Passalacqua <pjpassalacqua@Daniels-MacBook-Pro.local>
2015-05-27 13:16:32 -0400

    added pseudocode

---
+++ palindrome.py
@@ -0,0 +1,8 @@
+# Get user input.
+
+# Remove spaces, punctuation, and capitalization from the input
+
+# Algorithm
+    # Check to see if first and last characters are the same
+        # If they are, continue
+        # If they aren't, break
