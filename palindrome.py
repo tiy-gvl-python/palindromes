9ee3bca557e621b0b5d93275d3a8c1bc20f0d487
Bekk Blando <bekkblando@gmail.com>
2015-05-27 18:18:53 -0400

    This passes test.sh

---
+++ palindrome.py
@@ -0,0 +1,21 @@
+palindrome=input("What's your palindrome? ")
+
+def check_palindrome(palindrome):
+    #for letter in palindrome:
+    punc=['.',',',':',';',"'","_","!"]
+    for punc_remove in punc:
+        palindrome=palindrome.replace(punc_remove,'')
+    palindrome=palindrome.lower()
+    palindrome_reverse=palindrome[::-1]
+    palindrome_reverse=palindrome_reverse.replace(' ','')
+    palindrome=palindrome.replace(' ','')
+    if palindrome==palindrome_reverse:
+        print("is a palindrome")
+        print(palindrome)
+        print(palindrome_reverse)
+    else:
+        print(palindrome)
+        print(palindrome_reverse)
+        print("is not a palindrome")
+
+check_palindrome(palindrome)
