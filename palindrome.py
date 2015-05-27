94cd9085c212297b422ae2020d66ab015aa07adf
PJ Passalacqua <pjpassalacqua@Daniels-MacBook-Pro.local>
2015-05-27 15:57:51 -0400

    Changed printed output

--- palindrome.py
+++ palindrome.py
@@ -3,26 +3,24 @@ import re
 
 # Functions
 
+# Prints string_
+def print_string(string_):
+    print("Current string: {}".format(string_))
+
 # Takes a string and returns True if the string is a palindrome.
 def palindrome_check_recursive(string_):
-    print("Checking remaining length.")
+    print_string(string_)
     if len(string_) < 2:
-        print("Remaining string is less than 2 characters.")
         return True
-    print("Remaining string is {}.".format(string_))
-    print("Comparing first and last characters.")
     if string_.endswith(string_[0]):
-        print("First and last characters are the same.")
-        print("Stripping first and last characters from the string.")
         return palindrome_check_recursive(string_[1:-1])
-    print("First and last characters are different.")
     return False
 
 # Takes a string and returns all of the letters in lower case.
 def reformat(string_):
-    print("Removing all non-letters.")
+    print_string(string_)
     string_ = re.sub(r'[^A-Za-z]','',string_)
-    print("Making string lower case.")
+    print_string(string_)
     string_ = string_.lower()
     return string_
 
@@ -38,9 +36,7 @@ if not potential_palindrome_input:
 potential_palindrome = reformat(potential_palindrome_input)
 
 # Algorithm
-    # Compare first and last letter.
-    # If they are the same, then compare next letters
-print("Checking to see if you entered a palindrome.")
+print_string(potential_palindrome)
 is_a_palindrome = palindrome_check_recursive(potential_palindrome)
 
 if is_a_palindrome:
