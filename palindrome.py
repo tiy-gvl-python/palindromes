fe4d2b6e3012e570a1c247c7a1d9be69b2fb0741
Jeff Hacker <reachjkh@yahoo.com>
2015-07-21 01:18:21 -0400

    palindrome.py rewritten using function

--- palindrome.py
+++ palindrome.py
@@ -1,13 +1,18 @@
 # Check for Palindrome
 import re
 
-pal_input = input("Enter any word or sentence to see if it is a Palindrome.  ")
+pal_input = input("Enter any word or sentence (using quotes) to see if it is a Palindrome.  ")
 print(pal_input)
 pal_input = re.sub(r'[^A-Za-z]', "", pal_input.lower())
 print(pal_input)
 rev_input = pal_input[::-1]
 print(rev_input)
-if pal_input == rev_input:
-    print("Yes. Your text " + "is a palindrome.")
-else:
-    print("Nope.  That text " + "is not a palindrome.")
+
+
+def palindrome_check(pal_input, rev_input):
+    if pal_input == rev_input:
+        print("Yes. Your text is a palindrome.")
+    else:
+        print("Nope.  That text is not a palindrome.")
+
+palindrome_check(pal_input, rev_input)
