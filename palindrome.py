6c8202bb640c54db29ad5b84321310451dd92740
Bekk Blando <bekkblando@gmail.com>
2015-07-21 16:31:06 -0400

    Pep8

--- palindrome.py
+++ palindrome.py
@@ -1,15 +1,17 @@
-palindrome=input("What's your palindrome? ")
+
+palindrome = input("What's your palindrome? ")
+
 
 def check_palindrome(palindrome):
-    #for letter in palindrome:
-    punc=['.',',',':',';',"'","_","!"]
+    # for letter in palindrome:
+    punc = ['.', ',', ':', ';', "'", "_", "!"]
     for punc_remove in punc:
-        palindrome=palindrome.replace(punc_remove,'')
-    palindrome=palindrome.lower()
-    palindrome_reverse=palindrome[::-1]
-    palindrome_reverse=palindrome_reverse.replace(' ','')
-    palindrome=palindrome.replace(' ','')
-    if palindrome==palindrome_reverse:
+        palindrome = palindrome.replace(punc_remove, '')
+    palindrome = palindrome.lower()
+    palindrome_reverse = palindrome[::-1]
+    palindrome_reverse = palindrome_reverse.replace(' ', '')
+    palindrome = palindrome.replace(' ', '')
+    if palindrome == palindrome_reverse:
         print("is a palindrome")
         print(palindrome)
         print(palindrome_reverse)
